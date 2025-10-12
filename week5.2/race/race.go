8d2acde61fb66970e957372e8845907f42e7cf25
Puen Noah <109960620+puennoah@users.noreply.github.com>
2025-10-12 04:17:56 +0000

    Learning go on 12 Oct 25

---
+++ week5.2/race/race.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var count int
+	var wg sync.WaitGroup
+	// Start 10 goroutines
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go incrementCount(&count, &wg)
+	}
+	// Wait for all goroutines to finish
+	wg.Wait()
+
+	// Print the final count
+	fmt.Println("Final count:", count)
+
+}
+
+
+func incrementCount(count *int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Increment the count 1000 times
+	for i := 0; i < 1000; i++ {
+		*count++
+	}
+}
+
