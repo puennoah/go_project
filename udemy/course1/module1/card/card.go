1f384c0d01b4e0ffdd4d09725f395caccba99b78
Puen Noah <109960620+puennoah@users.noreply.github.com>
2026-01-08 05:39:23 +0000

    Learning go on 08/01/26

---
+++ udemy/course1/module1/card/card.go
@@ -0,0 +1,17 @@
+package main
+
+//import (
+//	"fmt"
+//)
+
+func main() {
+	cards := deck{newCard(), "Ace of Diamonds"}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
